2c3acc2943a866a7f146b75ec035532445c78d75
Maxim <maxeverent@mail.ru>
2022-12-19 19:16:33 +0300

    upd

--- exam/examController.js
+++ exam/examController.js
@@ -1,25 +1,42 @@
 const connect = require('../knex')
+const jwt = require('jsonwebtoken')
+const {secret} = require('../config')
 
 class doctorsController {
     async get(req, res) {
         try {
-            res.header("Access-Control-Allow-Origin", "*")
-            const exams = await connect.select("*").from("exam")
-            return res.status(200).json(exams)
+            const token = req.headers.authorization.split(' ')[1]
+            if (!token) {
+                return res.status(403).json({message: "Не авторизован"})
+            }
+            const {roles, username} = jwt.verify(token, secret)
+            if (roles == 2) {
+                const exams = await connect.select("*").from("exam")
+                return res.status(200).json(exams)
+            }
+            if (roles == 1) {
+                const exams = await connect.select("*").from("exam").where("username", username)
+                return res.status(200).json(exams)
+            }
         } catch(e) {
             console.log(e)
-            res.header("Access-Control-Allow-Origin", "*")
             return res.status(400).json("Error")
         }
     }
 
     async create(req, res) {
         try {
-            const {speciality, date, auth_token} = req.body
-            const exam = {speciality, date, auth_token}
-            await connect("exam").insert(exam)
+            const {speciality, date} = req.body
+            const token = req.headers.authorization.split(' ')[1]
+            if (!token) {
+                return res.status(403).json({message: "Не авторизован"})
+            }
+            const {username} = jwt.verify(token, secret)
+            console.log(username) 
+            const exams = {speciality, date, username}
+            await connect("exam").insert(exams)
             res.header("Access-Control-Allow-Origin", "*")
-            return res.status(200).json(exam)
+            return res.status(200).json(exams)
         } catch(e) {
             console.log(e)
             res.header("Access-Control-Allow-Origin", "*")
